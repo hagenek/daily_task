9a57a0405357b43a646bc783f28620ca75f99554
Georg Ekeberg <georghagen@gmail.com>
2025-06-12 15:14:52 +0200

    refactor: Redirect root to tasks page

--- lib/daily_task_web/controllers/page_controller.ex
+++ lib/daily_task_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule DailyTaskWeb.PageController do
   use DailyTaskWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/tasks")
   end
 end
